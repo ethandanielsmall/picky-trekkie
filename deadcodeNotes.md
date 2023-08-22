105e2e11107da41ce702f89dbbeb3066751f2e54
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-22 10:35:39 -0700

    init commit

---
+++ deadcodeNotes.md
@@ -0,0 +1,9 @@
+https://dba.stackexchange.com/questions/250866/is-this-the-correct-best-way-to-create-a-custom-column-in-mysql
+
+https://dev.mysql.com/doc/refman/8.0/en/show-columns.html
+
+https://dev.mysql.com/doc/refman/8.0/en/choosing-types.html
+
+https://github.com/ethandanielsmall/04-javascript-quiz/blob/main/assets/script.js
+
+https://github.com/ethandanielsmall/password-generator/blob/main/Develop/script.js
---
+++ script.js.js
@@ -0,0 +1,93 @@
+// TNG SEASON 1
+
+
+
+// TNG SEASON 2
+
+
+
+// TNG SEASON 3
+
+const seasonThreeCozy = []
+const seasonThreeHope = ["ensigns of command", "who watches the watchers", "the enemy", "deja Q", "the most toys"]
+const seasonThreePhili = ["who watches the watchers", "a matter of perspective", "sins of the father", "sarek"]
+const seasonThreeSpooky = ["the price"]
+const seasonThreeFunni = ["the enemy", "deja q", "allegiance", "captain's  holiday", "hollow pursuits", "menage a troi"]
+const seasonThreeSad = ["the offspring"]
+const seasonThreeOther = ["evolution", "the survivors", "the bonding", "the vengance factor", "the defector", "the hunted", "the high ground", "yesterday's enterprise", "tin man", "transfigurations" ]
+
+// TNG SEASON 4
+
+const seasonFourCozy = ['datas day', 'measure of a man']
+const seasonFourHope = ['datas day', 'measure of a man']
+const seasonFourPhili = ['datas day', 'measure of a man']
+const seasonFourSpooky = ['datas day', 'measure of a man']
+const seasonFourFunni = ['datas day', 'measure of a man']
+const seasonFourSad = ['datas day', 'measure of a man']
+const seasonFourOther = ['datas day', 'measure of a man']
+
+// TNG SEASON 5
+
+const seasonFiveCozy = ['datas day', 'measure of a man']
+const seasonFiveHope = ['datas day', 'measure of a man']
+const seasonFivePhili = ['datas day', 'measure of a man']
+const seasonFiveSpooky = ['datas day', 'measure of a man']
+const seasonFiveFunni = ['datas day', 'measure of a man']
+const seasonFiveSad = ['datas day', 'measure of a man']
+const seasonFiveOther = ['datas day', 'measure of a man']
+
+// TNG SEASON 6
+
+const seasonSixCozy = ['datas day', 'measure of a man']
+const seasonSixHope = ['datas day', 'measure of a man']
+const seasonSixPhili = ['datas day', 'measure of a man']
+const seasonSixSpooky = ['datas day', 'measure of a man']
+const seasonSixFunni = ['datas day', 'measure of a man']
+const seasonSixSad = ['datas day', 'measure of a man']
+const seasonSixOther = ['datas day', 'measure of a man']
+
+// TNG SEASON 7
+
+const seasonSevenCozy = ['datas day', 'measure of a man']
+const seasonSevenHope = ['datas day', 'measure of a man']
+const seasonSevenPhili = ['datas day', 'measure of a man']
+const seasonSevenSpooky = ['datas day', 'measure of a man']
+const seasonSevenFunni = ['datas day', 'measure of a man']
+const seasonSevenSad = ['datas day', 'measure of a man']
+const seasonSevenOther = ['datas day', 'measure of a man']
+
+// TNG SEASON 8
+
+const seasonEightCozy = ['datas day', 'measure of a man']
+const seasonEightHope = ['datas day', 'measure of a man']
+const seasonEightPhili = ['datas day', 'measure of a man']
+const seasonEightSpooky = ['datas day', 'measure of a man']
+const seasonEightFunni = ['datas day', 'measure of a man']
+const seasonEightSad = ['datas day', 'measure of a man']
+const seasonEightOther = ['datas day', 'measure of a man']
+
+
+function engage() {
+    // when choices 1, 2, and 3 are 'true'
+    // load 
+    console.log("engage")
+};
+
+engage();
+
+// types of vibes:
+
+// cozy -> cozy (cozy)
+// angry -> hope (justice served/hopeful)
+// bored -> phili (philisophical/eps that make you think)
+// spooky -> spooky (spooky/scary)
+// ??? -> funni (humerous)
+// depressed -> hope (justice served/hopeful)
+// like a downer -> sad (sad)
+// neuteral -> other (other)
+// i don't know -> other (other)
+
+// line of questioning
+// what series would you like to watch
+// what season do you prefer
+// how do you feel?
