bffa958a96e811625353c44d106d757c19a8c3a8
Ethan Daniel <e.daniel.small@gmail.com>
2023-12-29 19:25:31 -0700

    try to make intro page go away lol

--- script.js
+++ script.js
@@ -3,10 +3,14 @@
 // when the string is built, send it to the db
 // get back names of episodes and insert them onto page
 
+let sec = document.querySelector("#section")
+let timeout;
+
 function engage() {
     // when choices 1, 2, and 3 are 'true'
-    // load 
-    console.log("engage")
+    // load
+    sec.style.display="inline";
+    console.log("engage");
 };
 
-engage();
+document.getElementById("button").onclick = engage();
